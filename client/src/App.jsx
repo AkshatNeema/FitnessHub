9c94b940e6fec10df6bbd834dc64d11f1af6970a
Akshat Neema <akshatneema01@gmail.com>
2025-06-13 17:08:09 +0530

    navigation changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,42 +1,63 @@
-// import React from "react";
-// import { Routes, Route } from "react-router-dom";
-// import BMICalculator from "./pages/BMICalculator";
-
-// const App = () => {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<BMICalculator />} />
-//       <Route path="/login" element={<Login />} />
-//       <Route path="/signup" element={<Signup />} />
-//       {/* You can add more routes here later */}
-//     </Routes>
-//   );
-// };
-
-// export default App;
-
-
-// src/App.jsx
-import Navbar from "./components/Navbar";
+/* eslint-disable no-unused-vars */
+import * as React from 'react';
+import { Routes, Route } from "react-router-dom";
+import BMICalculator from "./pages/BMICalculator";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import BMICalculator from "./pages/BMICalculator"; // Replace if filename is different
+import Navbar from "./components/Navbar";
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
 
-import { Routes, Route, Router } from "react-router-dom";
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
 
-function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<BMICalculator />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </Router>
-  );
+  componentDidCatch(error, info) {
+    // eslint-disable-next-line no-undef
+    logErrorToMyService(
+      error,
+      // Example "componentStack":
+      //   in ComponentThatThrows (created by App)
+      //   in ErrorBoundary (created by App)
+      //   in div (created by App)
+      //   in App
+      info.componentStack,
+      // Warning: `captureOwnerStack` is not available in production.
+      React.captureOwnerStack(),
+    );
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
 }
 
+const App = () => {
+  return (
+    // <ErrorBoundary fallback={<p>Something went wrong</p>}>
+      // {/* <Navbar> */}
+      <>
+      <Navbar></Navbar>
+        <Routes>
+          <Route path="/" element={<BMICalculator />} />
+          <Route path="/login" element={<Login />}/>
+          <Route path="/signup" element= {<Signup />} />
+          {/* You can add more routes here later */}
+        </Routes>
+        </>
+      // {/* </Navbar> */}
+    // {/* </ErrorBoundary> */}
+  );
+};
 export default App;
 
 
