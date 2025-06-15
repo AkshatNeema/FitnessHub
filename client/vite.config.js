4b5dc1f32d07adb4c00a26900f1512f692765aa7
Akshat Neema <akshatneema01@gmail.com>
2025-06-15 11:47:42 +0530

    vercel plugins and vercel.json files

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,19 @@
 // client/vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 5173
+//   }
+// })
+import { defineConfig } from 'vite';
+import vercel from 'vite-plugin-vercel';
+ 
 export default defineConfig({
-  plugins: [react()],
   server: {
-    port: 5173
-  }
-})
+    port: 5173,
+  },
+  plugins: [vercel()],
+});
